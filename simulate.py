e0ea7e3dfe21d6e5d2436e33acfd9fcc8d20835c
ndunnett <nathandunnett@me.com>
2025-05-27 10:52:09 +0000

    minor optimisations

--- simulate.py
+++ simulate.py
@@ -12,33 +12,55 @@ from time import perf_counter_ns
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from collections.abc import Generator
     from pathlib import Path
 
 
-@dataclass
+@dataclass(frozen=True)
 class Team:
+    id: int
     name: str
     seed: int
     rating: tuple[int, ...]
 
-    def __repr__(self) -> str:
+    def __str__(self) -> str:
         return str(self.name)
 
     def __hash__(self) -> int:
-        return hash(self.name)
-
-    def __eq__(self, other: Team) -> bool:  # type: ignore
-        return self.name == other.name
+        return self.id
 
 
 @dataclass
 class Record:
     wins: int
     losses: int
+    teams_faced: set[Team]
+
+    @staticmethod
+    def new() -> Record:
+        return Record(wins=0, losses=0, teams_faced=set())
 
     @property
     def diff(self) -> int:
         return self.wins - self.losses
+
+
+@dataclass
+class Result:
+    three_zero: int
+    advanced: int
+    zero_three: int
+
+    @staticmethod
+    def new() -> Result:
+        return Result(three_zero=0, advanced=0, zero_three=0)
+
+    def __add__(self, other: Result) -> Result:
+        return Result(
+            three_zero=self.three_zero + other.three_zero,
+            advanced=self.advanced + other.advanced,
+            zero_three=self.zero_three + other.zero_three,
+        )
 
 
 @cache
@@ -56,15 +78,13 @@ def win_probability(a: Team, b: Team, sigma: tuple[int, ...]) -> float:
 class SwissSystem:
     sigma: tuple[int, ...]
     records: dict[Team, Record]
-    faced: dict[Team, set[Team]]
     remaining: set[Team]
-    finished: set[Team]
 
     def seeding(self, team: Team) -> tuple[int, int, int]:
         """Calculate seeding based on win-loss, Buchholz difficulty, and initial seed."""
         return (
             -self.records[team].diff,
-            -sum(self.records[opp].diff for opp in self.faced[team]),
+            -sum(self.records[opp].diff for opp in self.records[team].teams_faced),
             team.seed,
         )
 
@@ -93,15 +113,14 @@ class SwissSystem:
             self.records[team_b].wins += 1
 
         # add to faced teams
-        self.faced[team_a].add(team_b)
-        self.faced[team_b].add(team_a)
+        self.records[team_a].teams_faced.add(team_b)
+        self.records[team_b].teams_faced.add(team_a)
 
         # advance/eliminate teams after best of three
         if is_bo3:
             for team in [team_a, team_b]:
                 if self.records[team].wins == 3 or self.records[team].losses == 3:
                     self.remaining.remove(team)
-                    self.finished.add(team)
 
     def simulate_round(self) -> None:
         """Simulate round of matches."""
@@ -118,15 +137,16 @@ class SwissSystem:
 
         # first round is seeded differently (1-9, 2-10, 3-11 etc.)
         if len(even_teams) == len(self.records):
-            half = len(even_teams) // 2
-            even_teams[half:] = reversed(even_teams[half:])
+            for a, b in zip(even_teams, even_teams[len(even_teams) // 2 :]):
+                self.simulate_match(a, b)
 
         # run matches for each group, highest seed vs lowest seed
-        for group in [pos_teams, even_teams, neg_teams]:
-            half = len(group) // 2
+        else:
+            for group in [pos_teams, even_teams, neg_teams]:
+                second_half = reversed(group[len(group) // 2 :])
 
-            for a, b in zip(group[:half], reversed(group[half:])):
-                self.simulate_match(a, b)
+                for a, b in zip(group, second_half):
+                    self.simulate_match(a, b)
 
     def simulate_tournament(self) -> None:
         """Simulate entire tournament stage."""
@@ -143,27 +163,37 @@ class Simulation:
         with open(filepath) as file:
             data = json.load(file)
 
+        def id_generator() -> Generator[int]:
+            i = 0
+
+            while True:
+                yield i
+                i += 1
+
+        auto_id = id_generator()
         self.sigma = (*data["sigma"].values(),)
         self.teams = {
             Team(
-                team_k,
-                team_v["seed"],
-                tuple((eval(sys_v))(team_v[sys_k]) for sys_k, sys_v in data["systems"].items()),  # noqa: S307
+                id=next(auto_id),
+                name=team_k,
+                seed=team_v["seed"],
+                rating=tuple(
+                    (eval(sys_v))(team_v[sys_k])  # noqa: S307
+                    for sys_k, sys_v in data["systems"].items()
+                ),
             )
             for team_k, team_v in data["teams"].items()
         }
 
-    def batch(self, n: int) -> dict[Team, dict[str, int]]:
+    def batch(self, n: int) -> dict[Team, Result]:
         """Run batch of 'n' simulation iterations for given data and return results."""
-        results = {team: {stat: 0 for stat in ["3-0", "3-1 or 3-2", "0-3"]} for team in self.teams}
+        results = {team: Result.new() for team in self.teams}
 
         for _ in range(n):
             ss = SwissSystem(
-                self.sigma,
-                {team: Record(0, 0) for team in self.teams},
-                {team: set() for team in self.teams},
-                set(self.teams),
-                set(),
+                sigma=self.sigma,
+                records={team: Record.new() for team in self.teams},
+                remaining=set(self.teams),
             )
 
             ss.simulate_tournament()
@@ -171,41 +201,51 @@ class Simulation:
             for team, record in ss.records.items():
                 if record.wins == 3:
                     if record.losses == 0:
-                        results[team]["3-0"] += 1
+                        results[team].three_zero += 1
                     else:
-                        results[team]["3-1 or 3-2"] += 1
+                        results[team].advanced += 1
                 elif record.wins == 0:
-                    results[team]["0-3"] += 1
+                    results[team].zero_three += 1
 
         return results
 
-    def run(self, n: int, k: int) -> dict[Team, dict[str, int]]:
+    def run(self, n: int, k: int) -> dict[Team, Result]:
         """Run 'n' simulation iterations across 'k' processes and return results."""
         with Pool(k) as pool:
             futures = [pool.apply_async(self.batch, [n // k]) for _ in range(k)]
             results = [future.get() for future in futures]
 
-        def _f(acc: dict, res: dict) -> dict:
-            for team, result in res.items():
-                for key, val in result.items():
-                    acc[team][key] += val
+        def _f(acc: dict[Team, Result], results: dict[Team, Result]) -> dict[Team, Result]:
+            for team, result in results.items():
+                acc[team] += result
+
             return acc
 
         return reduce(_f, results)
 
 
-def format_results(results: dict[Team, dict[str, int]], n: int, run_time: float) -> list[str]:
-    """Formats simulation results and run time parameters into readable string."""
+def format_results(results: dict[Team, Result], n: int, run_time: float) -> list[str]:
+    """Formats simulation results and run time parameters into readable strings."""
+    fields = (
+        ("three_zero", "3-0"),
+        ("advanced", "3-1 or 3-2"),
+        ("zero_three", "0-3"),
+    )
+
     out = [f"RESULTS FROM {n:,} TOURNAMENT SIMULATIONS"]
 
-    for stat in next(iter(results.values())):
+    for attr, stat in fields:
         out.append(f"\nMost likely to {stat}:")
 
-        for i, (team, result) in enumerate(
-            sorted(results.items(), key=lambda tup: tup[1][stat], reverse=True),
-        ):
+        sorted_results = enumerate(
+            sorted(results.items(), key=lambda tup: getattr(tup[1], attr), reverse=True),
+        )
+
+        for i, (team, result) in sorted_results:
             out.append(
-                f"{str(i + 1) + '.':<3} {team.name:<18} {round(result[stat] / n * 100, 1):>5}%",
+                f"{str(i + 1) + '.':<3} "
+                f"{team.name:<18} "
+                f"{round(getattr(result, attr) / n * 100, 1):>5}%",
             )
 
     out.append(f"\nRun time: {run_time:.2f} seconds")
